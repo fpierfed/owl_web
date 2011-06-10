895a051197fc3e4208982d049c9285f31b935cdd
Francesco Pierfederici <fpierfed@me.com>
2011-06-10 14:32:51 -0400

    Added/hacked in support for job submission

---
+++ control/views.py
@@ -0,0 +1,146 @@
+# Copyright (C) 2010 Association of Universities for Research in Astronomy(AURA)
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+#     1. Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+# 
+#     2. Redistributions in binary form must reproduce the above
+#       copyright notice, this list of conditions and the following
+#       disclaimer in the documentation and/or other materials provided
+#       with the distribution.
+# 
+#     3. The name of AURA and its representatives may not be used to
+#       endorse or promote products derived from this software without
+#       specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY AURA ``AS IS'' AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL AURA BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+# OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+# TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+# USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+# DAMAGE.
+import os
+import subprocess
+
+from django.template import Context, loader
+from django.http import HttpResponse
+
+
+
+
+
+
+# Constants
+# TODO: We should be getting these from, say a DB.
+REPO_ROOT = '/jwdmsdevvm1/data1/owl_scratch/repository/raw'
+EXE = '/jwst/bin/process_dataset.py'
+
+# Format: ({'name': intrument+mode, 'datasets': (dataset1, dataset2, ...)}, )
+dataset001 = {'name': 'dataset_001', 
+              'exposures': ('raw-000001', 'raw-000002', 'raw-000003', 'raw-000004')}
+dataset002 = {'name': 'dataset_002', 
+              'exposures': ('raw-000001', 'raw-000002')}
+INSTRUMENTS = ({'name': 'instrument1/modeA', 
+                'datasets': (dataset001, dataset002)}, )
+
+
+
+
+
+def index(request):
+    """
+    Main entry point for the control web app.
+    """
+    # Load the template.
+    t = loader.get_template('control/index.html')
+    
+    # Render the template and exit.
+    c = Context({'instruments': INSTRUMENTS})
+    return(HttpResponse(t.render(c)))
+
+
+def dataset_index(request, instrument, mode):
+    """
+    Display available dataset exposures for the given instrument and mode.
+    """
+    # Load the template.
+    t = loader.get_template('control/dataset.html')
+    
+    name = str(instrument) + '/' + str(mode)
+    
+    # Find the instrument.
+    instr_dict = {'name': '%s not found' % (name),
+                  'datasets': ()}
+    for i in INSTRUMENTS:
+        if(i['name'] == name):
+            instr_dict = i
+            break
+    
+    # Render the template and exit.
+    c = Context({'instrument': instr_dict})
+    return(HttpResponse(t.render(c)))
+
+
+def process_index(request, instrument, mode, dataset, exposure):
+    """
+    Display available dataset exposures for the given instrument and mode.
+    """
+    # Load the template.
+    t = loader.get_template('control/process.html')
+    
+    name = str(instrument) + '/' + str(mode)
+    
+    # Find the instrument.
+    instr_dict = {'name': '%s not found' % (name),
+                  'datasets': ()}
+    for i in INSTRUMENTS:
+        if(i['name'] == name):
+            instr_dict = i
+            break
+    
+    # Get the repository path.
+    repo_path = os.path.join(REPO_ROOT, dataset)
+    
+    # Compose the command.
+    args = (EXE, '-r', repo_path, '-i', instrument, '-m', mode, exposure)
+    proc = subprocess.Popen(args, 
+                            stdout=subprocess.PIPE, 
+                            stderr=subprocess.PIPE,
+                            shell=False)
+    err = proc.wait()
+    
+    # Render the template and exit.
+    cmd_str = ' '.join(args)
+    c = Context({'command': cmd_str,
+                 'exit_code': err,
+                 'instrument': instr_dict,
+                 'dataset': dataset,
+                 'exposure': exposure})
+    return(HttpResponse(t.render(c)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- main/__init__.py
+++ main/__init__.py
@@ -26,34 +26,3 @@
 # TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 # USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
 # DAMAGE.
-from django.conf.urls.defaults import *
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
-urlpatterns = patterns('',
-    # Example:
-    # (r'^eunomia_web/', include('eunomia_web.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
-    
-    # Main entry point.
-    (r'^$', 'main.views.index'),
-    
-    # Monitor web app.
-    (r'^monitor/$', 'monitor.views.index'),
-    (r'^monitor/user/(?P<owner_name>\S+)/$', 'monitor.views.owner_index'),
-    (r'^monitor/dataset/(?P<dataset>\S+)/$', 'monitor.views.dataset_index'),
-    (r'^monitor/entry/(?P<entry_id>\S+)/$', 'monitor.views.entry_index'),
-    (r'^monitor/request/(?P<request_id>\d+)/$', 'monitor.views.request_index'),
-    
-    # Conreol web app.
-    (r'^control/$', 'control.views.index'),
-    (r'^control/(?P<instrument>[a-zA-Z0-9]+)/(?P<mode>[a-zA-Z0-9]+)/(?P<dataset>[a-zA-Z0-9_-]+)/(?P<exposure>[a-zA-Z0-9-]+)/$', 'control.views.process_index'),
-    (r'^control/(?P<instrument>[a-zA-Z0-9]+)/(?P<mode>[a-zA-Z0-9]+)/$', 'control.views.dataset_index'),
-)
